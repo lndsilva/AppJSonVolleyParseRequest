4098f4bbb7c5768570dcdd7ea9269e53c4a05a97
Laercio-Note <lndsilva@hotmail.com>
2021-04-11 15:10:52 -0300

    Initial commit

---
+++ app/src/main/java/br/com/local/appjsonvolleyparserequest/MainActivity.java
@@ -0,0 +1,78 @@
+package br.com.local.appjsonvolleyparserequest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView txtResult;
+    private RequestQueue mQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtResult = findViewById(R.id.txtResult);
+        Button btnParse = findViewById(R.id.btnParse);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        btnParse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                jsonParse();
+            }
+        });
+    }
+    private void jsonParse(){
+        String url = "http://192.168.100.16/heroapi/v1/Api.php?apicall=getheroes";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("heroes");
+                            for (int i=0; i < jsonArray.length(); i++){
+                                JSONObject heroe = jsonArray.getJSONObject(i);
+
+                                String name = heroe.getString("name");
+                                String realname = heroe.getString("realname");
+                                int rating = heroe.getInt("rating");
+                                String teamaffiliation = heroe.getString("teamaffiliation");
+
+                                txtResult.append(name + " - " + realname +" - "+ String.valueOf(rating)+ " - "+teamaffiliation+"\n\n");
+
+
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mQueue.add(request);
+    }
+}
